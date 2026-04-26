396bce36f21da2c8e77008085ea39f4580168a8a
Donghyeok Lim <jh07050@gmail.com>
2026-04-26 19:43:17 +0900

    feat(github): GithubModule + AppModule wire
    GithubApiClient 만 provider/export 하는 가벼운 모듈. AppModule 의 imports 에
    SecretsModule(글로벌, 토큰 공급) 다음 위치로 추가.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CairnModule } from './cairn/cairn.module.js';
 import { AppConfigModule } from './config/app-config.module.js';
+import { GithubModule } from './github/github.module.js';
 import { LoggingModule } from './logging/logging.module.js';
 import { SecretsModule } from './secrets/secrets.module.js';
 
 @Module({
-  imports: [AppConfigModule, LoggingModule, SecretsModule, CairnModule],
+  imports: [AppConfigModule, LoggingModule, SecretsModule, GithubModule, CairnModule],
 })
 export class AppModule {}
